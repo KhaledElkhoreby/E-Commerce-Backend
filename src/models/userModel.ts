76b4425c6b58abee788f4d063c1b57127059cecd
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-06-26 08:52:53 +0200

    Add User validation in Schema

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -6,12 +6,17 @@ export interface IUser {
   name: string;
   email: string;
   password: string;
+  passwordConfirm: string;
   address: IAddress[];
   cart: Types.ObjectId[];
   orders: Types.ObjectId[];
   role: 'admin' | 'user';
   paymentMethods: 'cash' | 'card';
   phone: string[];
+  passwordChangedAt: mongoose.Date;
+  passwordResetToken: String;
+  passwordResetExpires: mongoose.Date;
+  active: boolean;
 }
 
 const userSchema = new mongoose.Schema<IUser>({
@@ -21,14 +26,43 @@ const userSchema = new mongoose.Schema<IUser>({
   },
   email: {
     type: String,
-    required: [true, 'A user must have an email'],
+    require: [true, 'Please tell us your email!'],
     unique: true,
-    lowercase: true,
-    validate: [validator.isEmail, 'Please provide a valid email'],
+    trim: true,
+    lowerCase: true,
+    maxLength: [40, 'A user email must have less than or equal 40 characters'],
+    minLength: [
+      true,
+      'A user email must have more than or equal 10 characters',
+    ],
+    validate: [validator.isEmail, 'Please provide valid email'],
   },
   password: {
     type: String,
-    required: true,
+    require: [true, 'Please provide a password'],
+    trim: true,
+    minLength: [
+      8,
+      'A user password must have more than or equal to 8 characters',
+    ],
+    validate: [validator.isStrongPassword, 'Password is not strong'],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    require: [true, 'Please confirm your password'],
+    trim: true,
+    minLength: [
+      8,
+      'A user password must have more than or equal to 8 characters',
+    ],
+    // This only works on CREATE and SAVE !!!
+    validate: {
+      validator(val: string) {
+        return val === (this as unknown as IUser).password;
+      },
+      message: 'Please provide the same password',
+    },
   },
   address: [
     {
@@ -65,6 +99,20 @@ const userSchema = new mongoose.Schema<IUser>({
       required: true,
     },
   ],
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
+});
+
+// Middleware before QUERY
+userSchema.pre(/^find/, function (next) {
+  this.$model(this.modelName).find({ active: { $ne: false } });
+  return next();
 });
 
 const UserModel = mongoose.model<IUser>('user', userSchema);
