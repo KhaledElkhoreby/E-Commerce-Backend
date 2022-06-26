36539382cc3619acb68314e0e2582b5501ca6796
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-06-26 07:18:52 +0200

    Add calcAverageRatings static function for review

--- src/models/orderModel.ts
+++ src/models/orderModel.ts
@@ -2,44 +2,44 @@ import mongoose from 'mongoose';
 import { IProduct, productShecma } from './productModel';
 
 export interface IOrder {
-    products: IProduct[];
-    status:
-        | 'pendingPayment'
-        | 'preparing'
-        | 'shipping'
-        | 'completed'
-        | 'canceled';
-    totalPrice: number;
+  products: IProduct[];
+  status:
+    | 'pendingPayment'
+    | 'preparing'
+    | 'shipping'
+    | 'completed'
+    | 'canceled';
+  totalPrice: number;
 }
 
 const orderSchema = new mongoose.Schema<IOrder>(
-    {
-        products: [
-            {
-                type: productShecma,
-                required: true,
-            },
-        ],
-        status: {
-            type: String,
-            required: true,
-            enum: [
-                'pendingPayment',
-                'preparing',
-                'shipping',
-                'completed',
-                'canceled',
-            ],
-        },
-        totalPrice: {
-            type: Number,
-        },
+  {
+    products: [
+      {
+        type: productShecma,
+        required: true,
+      },
+    ],
+    status: {
+      type: String,
+      required: true,
+      enum: [
+        'pendingPayment',
+        'preparing',
+        'shipping',
+        'completed',
+        'canceled',
+      ],
     },
-    {
-        timestamps: {
-            createdAt: 'createdAt',
-        },
-    }
+    totalPrice: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: 'createdAt',
+    },
+  }
 );
 
 const OrderModel = mongoose.model<IOrder>('order', orderSchema);
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,66 +1,70 @@
 import mongoose, { Schema, Types } from 'mongoose';
 import addressSchema, { IAddress } from '../schemas/addressSchema';
+import validator from 'validator';
 
 export interface IUser {
-    name: string;
-    email: string;
-    password: string;
-    address: IAddress[];
-    cart: Types.ObjectId[];
-    orders: Types.ObjectId[];
-    role: 'admin' | 'user';
-    paymentMethods: 'cash' | 'card';
-    phone: string[];
+  name: string;
+  email: string;
+  password: string;
+  address: IAddress[];
+  cart: Types.ObjectId[];
+  orders: Types.ObjectId[];
+  role: 'admin' | 'user';
+  paymentMethods: 'cash' | 'card';
+  phone: string[];
 }
 
 const userSchema = new mongoose.Schema<IUser>({
-    name: {
-        type: String,
-        required: true,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have an email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: [
+    {
+      type: addressSchema,
+      required: true,
     },
-    email: {
-        type: String,
-        required: true,
+  ],
+  cart: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'product',
     },
-    password: {
-        type: String,
-        required: true,
+  ],
+  orders: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'order',
     },
-    address: [
-        {
-            type: addressSchema,
-            required: true,
-        },
-    ],
-    cart: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'product',
-        },
-    ],
-    orders: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'order',
-        },
-    ],
-    role: {
-        type: String,
-        required: true,
-        enum: ['user', 'admin'],
+  ],
+  role: {
+    type: String,
+    required: true,
+    enum: ['user', 'admin'],
+  },
+  paymentMethods: [
+    {
+      type: String,
+      enum: ['cash', 'card'],
     },
-    paymentMethods: [
-        {
-            type: String,
-            enum: ['cash', 'card'],
-        },
-    ],
-    phone: [
-        {
-            type: String,
-            required: true,
-        },
-    ],
+  ],
+  phone: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
 });
 
 const UserModel = mongoose.model<IUser>('user', userSchema);
