5592c92ab05301e116123ee2f32db110c08687c9
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-06-30 08:56:33 +0200

    Implement Authentication middleware

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,12 +1,13 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose, { Types } from 'mongoose';
 import UserModel, { IUser } from '../models/userModel';
 import AppError from '../utils/AppError';
 import catchAsync from '../utils/catchAsync';
 
-const SECRET_KEY = process.env.JWT_SECRET!;
+const SECRET_KEY = process.env.JWT_SECRET_KEY!;
 const EXPIRES_IN = process.env.JWT_EXPIRES_IN!;
+const JWT_COOKIE_EXPIRES_IN = process.env.JWT_COOKIE_EXPIRES_IN!;
 
 const signToken = (id: Types.ObjectId) =>
   jwt.sign({ id }, SECRET_KEY, { expiresIn: EXPIRES_IN });
@@ -16,10 +17,12 @@ const createSendToken = (
   statusCode: number,
   res: Response
 ) => {
-  const token = signToken(user._id as unknown as Types.ObjectId);
+  const token = signToken(user._id);
   //#region
   const cookieOptions = {
-    expires: new Date(Date.now() + +EXPIRES_IN * 24 * 60 * 60 * 1000),
+    expires: new Date(
+      Date.now() + +JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
     httpOnly: true,
     secure: false,
   };
@@ -65,5 +68,71 @@ export const login = catchAsync(
 
     // todo 2) check if user exists && password is correct
     const user = await UserModel.findOne({ email }).select('+password');
+
+    const isCorrect = await user?.isPasswordCorrect(password, user.password);
+
+    if (!user || !isCorrect)
+      return next(new AppError('Incorrect email or password', 401));
+
+    // todo 3) If everything ok, send token to client
+    return createSendToken(user, 200, res);
   }
 );
+
+export interface CustomRequest extends Request {
+  user?: {
+    id: Types.ObjectId;
+    role: IUser['role'];
+  };
+}
+
+export const product = catchAsync(
+  async (req: CustomRequest, res: Response, next: NextFunction) => {
+    // todo 1) Getting the token and check if it exit
+    let token;
+    if (req.headers?.authorization?.startsWith('Bearer')) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+    console.log({ token });
+
+    if (!token)
+      return next(
+        new AppError('You are not logged in! Please log in to get access.', 401)
+      );
+
+    // todo 2) Varification token
+    const decoded = jwt.verify(token, SECRET_KEY) as jwt.JwtPayload;
+    console.log({ decoded });
+
+    // todo 3) Check if user still exists
+    const currentUser = await UserModel.findById(decoded.id);
+    if (!currentUser)
+      return next(
+        new AppError(
+          'The user belonging to this token does no longer exist.',
+          401
+        )
+      );
+
+    // todo 4) Check if user changed password after the token was issued
+    if (currentUser.isPasswordChangedAfterThisToken(decoded.iat))
+      return next(
+        new AppError('The user changed password after this token', 401)
+      );
+
+    // ? GRANT ACCESS TO PRODUCTED ROUTE
+    req.user = { id: currentUser.id, role: currentUser.role };
+    return next();
+  }
+);
+
+export const restrictTo =
+  (...roles: IUser['role'][]) =>
+  (req: CustomRequest, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user?.role!)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
+    }
+    return next();
+  };
