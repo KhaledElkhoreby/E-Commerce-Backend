7ff666bf87b0e237726bed6cc5586346e903d291
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-07-21 20:54:22 +0200

    Clear worktrees

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -1,18 +1,14 @@
 import express from 'express';
 import {
-  addProduct,
-  deleteProduct,
-  getAllProduct,
-  getProduct,
-  updateProduct,
+    addProduct,
+    deleteProduct,
+    getAllProduct,
+    getProduct,
+    updateProduct,
 } from '../controllers/productController';
 const productsRouter = express.Router();
 
 productsRouter.route('/').get(getAllProduct).post(addProduct);
-productsRouter
-  .route('/:id')
-  .get(getProduct)
-  .patch(updateProduct)
-  .delete(deleteProduct);
+productsRouter.route('/:id').get(getProduct).patch(updateProduct).delete(deleteProduct);
 
 export default productsRouter;
