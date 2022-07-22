288a7166794bdecf6c5ea993bd6ba0c25d439be6
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-07-22 19:39:01 +0200

    Manage permessions for reviews routes

--- src/routes/reviewRoute.ts
+++ src/routes/reviewRoute.ts
@@ -1,14 +1,23 @@
 import express from 'express';
+import { protect, restrictTo } from '../controllers/authController';
 import {
-    addReview,
-    deleteReview,
-    getAllReview,
-    getReview,
-    updateReview,
+  addReview,
+  deleteReview,
+  getAllReview,
+  getReview,
+  updateReview,
 } from '../controllers/reviewController';
+
 const reviewsRouter = express.Router();
 
-reviewsRouter.route('/').get(getAllReview).post(addReview);
-reviewsRouter.route('/:id').get(getReview).patch(updateReview).delete(deleteReview);
+reviewsRouter.use(protect);
+
+reviewsRouter.route('/').get(getAllReview).post(restrictTo('user'), addReview);
+
+reviewsRouter
+  .route('/:id')
+  .get(getReview)
+  .patch(restrictTo('admin', 'user'), updateReview)
+  .delete(restrictTo('admin', 'user'), deleteReview);
 
 export default reviewsRouter;
