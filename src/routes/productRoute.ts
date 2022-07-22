48624946b28c2f8c363bfecd5c88b33a68fec7aa
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-07-22 19:38:26 +0200

    Mange permission for product routes

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -1,14 +1,22 @@
 import express from 'express';
+import { protect, restrictTo } from '../controllers/authController';
 import {
-    addProduct,
-    deleteProduct,
-    getAllProduct,
-    getProduct,
-    updateProduct,
+  addProduct,
+  deleteProduct,
+  getAllProduct,
+  getProduct,
+  updateProduct,
 } from '../controllers/productController';
 const productsRouter = express.Router();
 
-productsRouter.route('/').get(getAllProduct).post(addProduct);
-productsRouter.route('/:id').get(getProduct).patch(updateProduct).delete(deleteProduct);
+productsRouter
+  .route('/')
+  .get(getAllProduct)
+  .post(protect, restrictTo('admin'), addProduct);
+productsRouter
+  .route('/:id')
+  .get(getProduct)
+  .patch(protect, restrictTo('admin'), updateProduct)
+  .delete(protect, restrictTo('admin'), deleteProduct);
 
 export default productsRouter;
