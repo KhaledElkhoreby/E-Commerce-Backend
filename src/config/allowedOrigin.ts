2f1daffce1af59551dd7e3f63bed976f8df3e811
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-08-12 21:20:38 +0200

    Add Vercel domain name

--- src/config/allowedOrigin.ts
+++ src/config/allowedOrigin.ts
@@ -2,6 +2,7 @@ export const allowedOrigins = [
   'https://fierce-shelf-11530.herokuapp.com',
   'https://e-commerce-frontend-h7r1aor5u-khaledelkhoreby.vercel.app/',
   'https://e-commerce-frontend-drab.vercel.app/',
+  'https://my-vastra.herokuapp.com',
   'http://127.0.0.1:5500',
   'http://localhost:3500',
   'http://localhost:3000',
